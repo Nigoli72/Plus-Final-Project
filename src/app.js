b925063bc4e380d8961ede4887309733fdeafae2
Nigoli72 <125713703+Nigoli72@users.noreply.github.com>
2023-02-20 10:53:23 -0500

    inserted and styled placeholder icon

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   if (hours < 10) {
-    hours = 0`${hours}`;
+    hours = "0`${hours}`";
   }
   let minutes = date.getMinutes();
   if (minutes < 10) {
-    minutes = 0`${minutes}`;
+    minutes = "0`${minutes}`";
   }
   let days = [
     "Sunday",
@@ -22,6 +22,7 @@ function formatDate(timestamp) {
 }
 
 function displayWeather(response) {
+  console.log(response);
   let temperatureElement = document.querySelector("#currentTemperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#currentCity");
@@ -34,9 +35,9 @@ function displayWeather(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#time");
   dateElement.innerHTML = formatDate(response.data.time * 1000);
+  let iconElement = document.querySelector("#image");
 }
 
 let apiKey = "74ft7426o38737ab0c3021aae5a380df";
 let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayWeather);
