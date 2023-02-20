c639894f194d58f5c78c972197242140acd894ca
Nigoli72 <125713703+Nigoli72@users.noreply.github.com>
2023-02-19 23:40:32 -0500

    timestamp codes-not yet displayed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
-let apiKey = "74ft7426o38737ab0c3021aae5a380df";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=${apiKey}&units=metric`;
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = 0`${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = 0`${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
+}
 function displayWeather(response) {
-  console.log(response);
   let temperatureElement = document.querySelector("#currentTemperature");
   temperatureElement.innerHTML = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#currentCity");
@@ -13,5 +32,8 @@ function displayWeather(response) {
   let windElement = document.querySelector("#windSpeed");
   windElement.innerHTML = Math.round(response.data.wind.speed);
 }
+
+let apiKey = "74ft7426o38737ab0c3021aae5a380df";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?lat=38.71667&lon=-9.13333&key=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayWeather);
